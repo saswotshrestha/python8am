8a367110e97079385ce2015a6211be46ab1e0286
saswotshrestha <saswotsa@gmail.com>
2023-05-01 16:49:27 -0700

    update

---
+++ main.py
@@ -0,0 +1,40 @@
+# print("hello")
+# print(45)
+
+# x=10
+
+# print(x)
+
+# name = input("Enter name: ")
+# print(name)
+
+#x=10
+#y=20
+
+#if x>y:
+ #   print("x is large")
+
+#else:
+ #   print("y is large")
+
+# age = int(input("Enter age: "))
+# if age >= 18 and age <= 40:
+#     print("you can join the party")
+#     if age >= 18 and age <= 25:
+#         print ("you can drink coke")
+#     elif age > 25:
+#         print ("you can drink beer")
+# else:
+#     print("you cannot join the party")
+
+x = 7
+y = 8
+z = 9
+if x > y:
+    if x > z:
+        print ("x is greatest")
+if y > z:
+    if y>x:
+        print ("y is greater")
+else:
+    print ("z is greater")
